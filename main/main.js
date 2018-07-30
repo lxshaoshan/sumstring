5941e187eb4917ac6effa5a996a51e4b5b7c57a3
lxshaoshan <lxshaoshan@126.com>
2018-07-30 18:49:45 +0800

    string

--- main/main.js
+++ main/main.js
@@ -1,3 +1,8 @@
-module.exports = function main() {
+module.exports = function main(strNum) {
   // Write your code here
+  let result = 0;
+  for (let str of strNum) {
+	result += parseInt(str);
+  }
+  return result;
 };
